d5fe192a43e4eb000b5773c9bebf0fdbf361e86c
sarthak bhagat <sarthakbhagat@gmail.com>
2018-03-20 02:09:58 +0530

    vae with different func

--- mnist_autoencoder/Variational_Autoencoder/vae.py
+++ mnist_autoencoder/Variational_Autoencoder/vae.py
@@ -7,8 +7,12 @@ from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from torchvision.utils import save_image
+from torchvision import datasets
 from torchvision.datasets import MNIST
 import os
+import random
+
+array = []
 
 if not os.path.exists('./vae_img'):
     os.mkdir('./vae_img')
@@ -29,9 +33,11 @@ img_transform = transforms.Compose([
     # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-dataset = MNIST('./data', transform=img_transform, download=True)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+#dataset = MNIST('./data', transform=img_transform, download=True)
+#dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
+def write(file):
+    
 
 class VAE(nn.Module):
     def __init__(self):
@@ -67,11 +73,8 @@ class VAE(nn.Module):
 
 
 model = VAE()
-if torch.cuda.is_available():
-    model.cuda()
 
 reconstruction_function = nn.MSELoss(size_average=False)
-
 
 def loss_function(recon_x, x, mu, logvar):
     """
@@ -90,32 +93,105 @@ def loss_function(recon_x, x, mu, logvar):
 
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-for epoch in range(num_epochs):
-    model.train()
-    train_loss = 0
-    for batch_idx, data in enumerate(dataloader):
-        img, _ = data
-        img = img.view(img.size(0), -1)
-        img = Variable(img)
-        if torch.cuda.is_available():
-            img = img.cuda()
-        optimizer.zero_grad()
-        recon_batch, mu, logvar = model(img)
-        loss = loss_function(recon_batch, img, mu, logvar)
-        loss.backward()
-        train_loss += loss.data[0]
-        optimizer.step()
-        if batch_idx % 100 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch,
-                batch_idx * len(img),
-                len(dataloader.dataset), 100. * batch_idx / len(dataloader),
-                loss.data[0] / len(img)))
+def train(batchsize):
+    train_set = torch.utils.data.DataLoader(datasets.MNIST('./data',train=True,download=True,transform=transforms.ToTensor()),batch_size=batchsize, shuffle=True)
+    
+    for epoch in range(num_epochs):
+        model.train()
+        train_loss = 0
+        for batch_idx, data in enumerate(train_set):
+            img, _ = data
+            img = img.view(img.size(0), -1)
+            img = Variable(img)
+            if torch.cuda.is_available():
+                img = img.cuda()
+            optimizer.zero_grad()
+            recon_batch, mu, logvar = model(img)
+            loss = loss_function(recon_batch, img, mu, logvar)
+            loss.backward()
+            train_loss += loss.data[0]
+            optimizer.step()
+            if batch_idx % 100 == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch,
+                    batch_idx * len(img),
+                    len(train_set.dataset), 100. * batch_idx / len(train_set),
+                    loss.data[0] / len(img)))
+                array.append([epoch,len(train_set.dataset),loss.data[0] / len(img), 100. * batch_idx / len(train_set)])
 
-    print('====> Epoch: {} Average loss: {:.4f}'.format(
-        epoch, train_loss / len(dataloader.dataset)))
-    if epoch % 10 == 0:
-        save = to_img(recon_batch.cpu().data)
-        save_image(save, './vae_img/image_{}.png'.format(epoch))
+        print('====> Epoch: {} Average loss: {:.4f}'.format(
+            epoch, train_loss / len(train_set.dataset)))
+        if epoch % 10 == 0:
+            save = to_img(recon_batch.cpu().data)
+            save_image(save, './vae_img/image_{}.png'.format(epoch))
 
+    file = open(“testfile.txt”,”w”) 
+    write(file)
+    file.close()
+
+    return model
+
+def test(model,batchsize):
+    test_set=torch.utils.data.DataLoader(datasets.MNIST('./data',train=False,download=True,transform=transforms.ToTensor(),batch_size=batchsize,shuffle=False))
+    r=random.randint(test_set.dataset)
+    test_image=test_set.dataset[r][0]
+    input_image=test_image.view(28,28).numpy()
+    out, mu1, logvar1 = model(Variable(test_image))
+    output_image = out.view(28,28).data.numpy()
+
+    plot_image = np.concatenate((input_image, output_image), axis=1) 
+    plt.imshow(plot_image, cmap='gray', interpolation='nearest');
+    plt.show()
+    
+
+model = train(batchsize = batch_size)
+test(model,batchsize = batch_size)
 torch.save(model.state_dict(), './vae.pth')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
